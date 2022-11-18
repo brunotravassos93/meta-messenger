84355bccf5eb60cdb67ad52c24b2b71e43062906
Bruno Travassos <brunnomoraaes@gmail.com>
2022-11-18 10:46:00 -0300

    adicionado radis e uuid

--- app/LogoutButton.tsx
+++ app/LogoutButton.tsx
@@ -4,7 +4,7 @@ function LogoutButton() {
   return (
     <button
       onClick={() => console.log("hello")}
-      className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4"
+      className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
     >
       Sign Out
     </button>
